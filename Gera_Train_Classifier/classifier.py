1dc8adc93e8a2c398a6b0df7c4d393f8fadc3d32
Adam Yala <me@adamyala.io>
2015-11-30 16:01:49 -0500

    Added test.json file. Got JSON parsing and feature extraction / output extraction mostly working

--- Gera_Train_Classifier/classifier.py
+++ Gera_Train_Classifier/classifier.py
@@ -5,3 +5,26 @@ import json
 
 np.random.seed(1)
 
+#Label Classes
+NORMAL = 0
+FALL   = 1
+
+def parseFeature(sample):
+    return sample["feature_vector"].split(" ")
+
+def parseOutput(sample):
+    return NORMAL if sample["sample_type"] == "Normal" else FALL
+
+##
+## Read data and load JSON
+##
+labeledSamples = []
+with open("raw_data.json", "r") as dataFile:
+    labeledSamples = json.load(dataFile)["results"]
+
+##
+## Split up JSON into input features and outputs
+##
+inputFeatures = [parseFeature(sample) for sample in labeledSamples[:2]]
+output        = [parseOutput(sample) for sample in labeledSamples[:2]]
+
