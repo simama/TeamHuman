6d64b4852f23c158c54a9d71dbe9bb683035196f
Adam Yala <me@adamyala.io>
2015-12-01 01:59:30 -0500

    Split data into train vs test

--- Gera_Train_Classifier/classifier.py
+++ Gera_Train_Classifier/classifier.py
@@ -2,12 +2,16 @@ import sys
 import numpy as np
 import neurolab as nl
 import json
+import random
 
 np.random.seed(1)
 
 #Label Classes
 NORMAL = 0
 FALL   = 1
+
+#Training Params
+trainingProp = .8
 
 def parseFeature(sample):
     return [ 0 if s == "" else float(s) for s in sample["feature_vector"].split(" ") ]
@@ -27,4 +31,19 @@ with open("raw_data.json", "r") as dataFile:
 ##
 inputFeatures = [parseFeature(sample) for sample in labeledSamples]
 output        = [parseOutput(sample) for sample in labeledSamples]
+
+
+##
+## Split Train / Test
+##
+numSamples = len(output)
+trainingSetSize = int(numSamples * trainingProp)
+allIds = range(len(output))
+
+
+trainIdSet = np.array(random.sample(allIds, trainingSetSize))
+testIdSet = np.setdiff1d(allIds, trainIdSet)
+
+trainInput, trainOutput = np.array([inputFeatures[i] for i in trainIdSet]), np.array([output[i] for i in trainIdSet])
+testInput, testOutput = np.array([inputFeatures[i] for i in testIdSet]), np.array([output[i] for i in testIdSet])
 
