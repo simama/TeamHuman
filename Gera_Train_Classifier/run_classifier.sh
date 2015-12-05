cda1c06e83e7b93a0932214a5955061ad5559cad
Adam Yala <adamyala@mit.edu>
2015-12-05 16:28:41 -0500

    Patched classifer script to delete solutions on start

--- Gera_Train_Classifier/run_classifier.sh
+++ Gera_Train_Classifier/run_classifier.sh
@@ -4,13 +4,14 @@ function run_all {
     COUNTER=0
     RANGE=1000
     while [  $COUNTER -lt $RANGE ]; do
-        python classifier.py --no_progress_bar --serialize 9 -s solutions/olution_$COUNTER.prm
+        python classifier.py --no_progress_bar --serialize 9 -s solutions/solution_$COUNTER.prm
         let COUNTER=COUNTER+1 
     done
 }
 
-rm output;
+rm output.tmp;
 rm error.log
-echo "Starting to run 100 iterations. Numbers will posted in output.tmp"
+rm solutions/*
+echo "Starting to run 1000 iterations. Numbers will posted in output.tmp"
 run_all 1> output.tmp 2>error.log &
 
